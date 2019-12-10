7a36600c64557257fc806df55d7422dc9e70ca80
Doryan Lièvre <doryan.lievre@edu.itescia.fr>
2019-12-10 16:24:44 +0100

    src : add binaryListToBinaryString.py

--- src/listOfIntToBinaryNumber.py
+++ src/listOfIntToBinaryNumber.py
@@ -1,12 +1,17 @@
+"""Convert a list of int to a list of binary number on 8 bit
+"""
+
 def list_of_int_to_binary_numbers(list):
     """ Convert a list of int to a list of binary number on 8 bit
 
     Args:
-        list:
+        list: A list of int to convert to a list of binary numbers
 
     Returns:
+        binary_list: A list of binary numbers
 
     """
+
     binary_list =[]
     for element in list:
         binary_list.append(format(element, '08b'))
