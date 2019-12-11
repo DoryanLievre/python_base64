3026986cfb600abe4af3c6008e3f72d234d65099
Doryan Lièvre <doryan.lievre@edu.itescia.fr>
2019-12-11 09:09:26 +0100

    src : rename functions

--- base64codecc.py
+++ base64codecc.py
@@ -1,7 +1,20 @@
 from src import *
 
 def main():
-    pass
+    print('Base 64 encoder.')
+    while True :
+        user_input = input('Enter a string to encode or e(X)it: ')
+        #
+        #Exit if user types 'x' or 'X'
+        #
+        if user_input.upper() == 'X':
+            print('Exiting Base 64 encoder')
+            break
+        #
+        #Calling the first function (step #1 of the algorithm)
+        #
+        encoded_string = stringToList(user_input)
+        print(f'{encoded_string}')
 
 if __name__ == '__main__':
     main()
